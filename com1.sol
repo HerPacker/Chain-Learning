c8ad13bcf6ac5cf3a70042cc8302de6ceaa55951
Jingjing Wang <90374799+HerPacker@users.noreply.github.com>
2021-09-09 06:19:07 +0000

    Create com1.sol

---
+++ com1.sol
@@ -0,0 +1,8 @@
+Designing Data-Intensive Applications - The Big Ideas Behind Reliable, Scalable, and Maintainable Systems
+https://rapidgator.net/file/0ea9040ff6abe0c9b7b03c29b8027a32
+
+Own Your Tech Career - Soft skills for technologists
+https://rapidgator.net/file/6c79a99de00bc5499a976326a33a78b8
+
+SQL Pocket Guide - A Guide to SQL Usage - Fourth Edition
+https://rapidgator.net/file/ecc646c354302a2e36f4a44fa712d8a9
